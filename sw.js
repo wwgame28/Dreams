3ec049d0c2dbc16f6f957b7b8fdaff0f9f38ecc6
wwgame28 <neimaambachew@gmail.com>
2026-07-31 12:37:03 +0900

    Refresh PWA cache for redesigned Dreams screen

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'after-the-end-v1';
+const CACHE = 'dreams-v3';
 const ASSETS = [
   './',
   './index.html',
@@ -23,10 +23,12 @@ self.addEventListener('activate', (event) => {
 self.addEventListener('fetch', (event) => {
   if (event.request.method !== 'GET') return;
   event.respondWith(
-    caches.match(event.request).then((cached) => cached || fetch(event.request).then((response) => {
-      const copy = response.clone();
-      caches.open(CACHE).then((cache) => cache.put(event.request, copy));
-      return response;
-    }).catch(() => caches.match('./index.html')))
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE).then((cache) => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request).then((cached) => cached || caches.match('./index.html')))
   );
 });
